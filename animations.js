29d58ef499eed937614902f8a106e51500f2f255
Alessio Maffeis <maffo88@gmail.com>
2013-04-06 17:19:08 +0200

    Gaaaa!

--- animations.js
+++ animations.js
@@ -5,14 +5,12 @@ $(window).bind('scroll',function(e){
 function parallax()
 {
     var scrolled = $(window).scrollTop();
-    $('#logo').css('top',(221+(scrolled*.5))+'px');
-    $('#phone').css('top',(56+(scrolled*.9))+'px');
-
-    if(scrolled>370)
+   
+    if(scrolled<20)
     {
-        $('#divHead').css('position', 'fixed');
-        $('#divHead').css('top', '-230px');
-        $('#divHead').css('left', 'auto');
+	    $('#logo').css('top',(221+(scrolled*.5))+'px');
+	    $('#phone').css('top',(56+(scrolled*.9))+'px');
     }
+
 }
 
