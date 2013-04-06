271522b18f3704984d93bcaf526c0abe0a216563
Alessio Maffeis <maffo88@gmail.com>
2013-04-06 17:13:59 +0200

    Work in progress #3

--- animations.js
+++ animations.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
+$(window).bind('scroll',function(e){
+    parallax();
+});
 
-// do nasty things here.
+function parallax()
+{
+    var scrolled = $(window).scrollTop();
+    $('#logo').css('top',(221+(scrolled*.5))+'px');
+    $('#phone').css('top',(56+(scrolled*.9))+'px');
 
+    if(scrolled>370)
+    {
+        $('#divHead').css('position', 'fixed');
+        $('#divHead').css('top', '-230px');
+        $('#divHead').css('left', 'auto');
+    }
 }
+
